38554e8f43ef87fcbddf865843853dc8a3a264f3
Sijin Joseph <sijin.joseph@campuslogic.com>
2022-03-11 18:16:21 -0500

    Use role policy attachment
    this will prevent staging and prod from fighting over each other.

--- housekeep.tf
+++ housekeep.tf
@@ -62,7 +62,7 @@ data "aws_iam_policy_document" "lambda_assume" {
   }
 }
 
-resource "aws_iam_policy_attachment" "housekeep_lambda" {
+resource "aws_iam_role_policy_attachment" "housekeep_lambda" {
   count      = var.storage_retention_days == null ? 0 : 1
   name       = "${local.name_prefix}jaeger-housekeep"
   roles      = [aws_iam_role.jaeger_housekeep[0].name]
