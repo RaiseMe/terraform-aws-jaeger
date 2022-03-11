8ecdb5c62190aee1ddf2bbc03e0b66e3bde802e7
Sijin Joseph <sijin.joseph@campuslogic.com>
2022-03-11 18:17:59 -0500

    Fix attribute

--- housekeep.tf
+++ housekeep.tf
@@ -65,7 +65,7 @@ data "aws_iam_policy_document" "lambda_assume" {
 resource "aws_iam_role_policy_attachment" "housekeep_lambda" {
   count      = var.storage_retention_days == null ? 0 : 1
   name       = "${local.name_prefix}jaeger-housekeep"
-  roles      = [aws_iam_role.jaeger_housekeep[0].name]
+  role      = [aws_iam_role.jaeger_housekeep[0].name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
